8594aa0f10517fe13260522255cf64fec1ae8cbe
unknown <peters0470@gmail.com>
2020-11-21 18:10:12 -0600

    added questions to index

--- index.js
+++ index.js
@@ -1,5 +1,61 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
 // array of questions for user
 const questions = [
+        {
+          type: "input",
+          name: "name",
+          message: "What is your project's name?",
+        },
+        {
+          type: "list",
+          name: "license",
+          message: "Apply License Badge?",
+          choices: [
+            new inquirer.Separator(),
+            "MIT",
+            new inquirer.Separator(),
+            "GPLv3",
+            new inquirer.Separator(),
+            "AGPL",
+            new inquirer.Separator(),
+          ],
+        },
+        {
+          type: "input",
+          name: "description",
+          message: "Briefly, describe your project.",
+        },
+      
+        {
+          type: "editor",
+          name: "installationInstructions",
+          message:
+            "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
+        },
+        {
+          type: "input",
+          name: "test",
+          message: "What is the command to test the project?",
+        },
+        {
+          type: "input",
+          name: "gitHub",
+          message: "What's your GitHub username?",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "What is your email address?",
+        },
+        {
+          type: "input",
+          name: "contributors",
+          message: "Who are the contributors of this project?",
+        },
+      
 
 ];
 
